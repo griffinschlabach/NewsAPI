812cdb6ae503508bfa8ab201db5a99d8a8bc39b3
Griffin Schlabach <griffin.schlabach@gmail.com>
2024-12-10 14:35:47 -0500

    Initial Commit

---
+++ NewsAPI/NewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAPIApp.swift
+//  NewsAPI
+//
+//  Created by Griffin Schlabach (student LM) on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
